3a1157737c5a4b9bb37246d1cbdcd83eeaf9a8dd
卫慧杰 <w920098695@sina.cn>
2021-07-27 22:42:48 +0800

    更新模板

--- schemas/PostsSchema.js
+++ schemas/PostsSchema.js
@@ -7,7 +7,10 @@ const PostSchema = new Schema({
   postedBy: {
     type: Schema.Types.ObjectId, ref: "User"
   },
-  likes: [{ type: Schema.Types.ObjectId, ref: "User" }]
+  likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  // 信息的角度考虑： 1. 那条消息被转发了，2. 被谁转发了
+  retweetDatas: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+  retweetUsers: [{ type: Schema.Types.ObjectId, ref: "User" }]
 }, { timestamps: true });
 
 var Post = mongoose.model('Post', PostSchema);
--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -8,7 +8,9 @@ const UserSchema = new Schema({
   email: { type: String, required: true, trim: true, unique: true },
   password: { type: String, required: true },
   profilePic: { type: String, default: "/images/profilePic.png" },
-  likes: [{ type: Schema.Types.ObjectId, ref: "Post" }]
+  likes: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+  // 从用户角度考虑: 该用户转发了那条消息
+  retweets: [{ type: Schema.Types.ObjectId, ref: "Post" }],
 }, { timestamps: true });
 
 var User = mongoose.model('User', UserSchema);
